78b3b8a6901360c584dba13ed696f5cbcda65d51
falke <falke.cs@gmail.com>
2017-02-10 20:57:45 +0100

    Implemented pfx-key generation script for Emby SSL-cert

--- scripts/pubpfxgen.sh
+++ scripts/pubpfxgen.sh
@@ -1,1 +1,1 @@
-/var/lib/emby/ssl/pfxgen.sh
+openssl pkcs12 -export -in /etc/letsencrypt/live/cogitantium.com/fullchain.pem -inkey /etc/letsencrypt/live/cogitantium.com/privkey.pem -out /var/lib/emby/ssl/fullcert.pfx
