496f2d97af60e7e53d4616cc47871bb3e06db7a0
falke <falke.cs@gmail.com>
2017-02-10 21:02:56 +0100

    Implemented permission-script for media-locations

--- scripts/permissionmedia.sh
+++ scripts/permissionmedia.sh
@@ -1,1 +1,4 @@
-permissionmedia.sh
+#!/bin/bash
+chown serv:emby -R /mnt/disk/0_movies/ /mnt/disk/1_tvshows/ /home/serv/Downloads/Torrents/Finished/0_movies/ /home/serv/Downloads/Torrents/Finished/1_tvshows/ /mnt/disk3/0_movies/ /mnt/disk3/1_tvshows/
+chmod 770 -R /mnt/disk/0_movies/ /mnt/disk/1_tvshows/ /home/serv/Downloads/Torrents/Finished/0_movies/ /home/serv/Downloads/Torrents/Finished/1_tvshows/ /mnt/disk3/0_movies/ /mnt/disk3/1_tvshows/
+echo 'Media chown/chmod for serv:emby with 770 permissions'
